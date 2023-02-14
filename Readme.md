6ce6a2c08a10866cfbdbed7ea949929382e934b8
ilovetiktiok <alexdubovyck@yandex.ru>
2023-02-14 21:51:39 +0300

    1-st commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+<!-- https://gitflic.ru/project/ilovetiktiok/amanda-lizard -->
+
+# Бот пишет "человек слаб" в случайное время, каждый день
+
+# узнать chat id "открытого канала"
+1) @twitch_clipz chat id -1001604597243
+2) https://lavrynenko.com/get_id_from_telegram.php
+3) https://api.telegram.org/bot TOKEN /getChat?chat_id=@twitch_clipz
+
+# узнать chat id "закрытого канала"
+https://t.me/c/1670463029/22766 и добавить -100 в начало
+<!-- скопировать ссылку на пост и прибавить 100 в начало -->
+<!-- 1670463029 -->
+<!-- -100 1670463029 -->
+
+# чат ID "Зазеркалья"
+CHAT_ID = -1001670463029
+
+# война с CMD
+set TELEGRAM_BOT_TOKEN="123" # без пробелов после = !!! # https://www3.ntu.edu.sg/home/ehchua/programming/howto/Environment_Variables.html
+echo %TELEGRAM_BOT_TOKEN%
+cd /D "E:\YandexDisk\Python\[2023] Amanda Lizard\"# Amanda-Lizard
---
+++ mirror.py
@@ -0,0 +1,43 @@
+import os, time, requests, random
+# from lib_1 import TOKEN
+
+TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
+print("TOKEN: ", TOKEN) # github
+
+CHAT_ID = -1001670463029 # twitch_clipz (от Самой Аманды) https://t.me/c/1670463029/22766 + -100
+text_for_send = "человек слаб"
+
+random_time_list1 = [5, 6, 7, 8]
+random_time_list2 = [11, 12, 13, 14]
+random_time_list3 = [18, 19, 20, 22, 23]
+choice1 = random.choice(random_time_list1)
+choice2 = random.choice(random_time_list2)
+choice3 = random.choice(random_time_list3)
+
+def Send_to_zazerkalie():
+    requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={text_for_send}")
+    # responce = req.json() # print(responce)
+
+def run():
+    while True:
+        current_time = time.localtime()
+        h, m, s = current_time.tm_hour, current_time.tm_min, current_time.tm_sec
+
+        # task
+        if (h == choice1 and m == 0 and s == 0):
+            Send_to_zazerkalie()
+
+        if (h == choice2 and m == 0 and s == 0):
+            Send_to_zazerkalie()
+
+        if (h == choice3 and m == 0 and s == 0):
+            Send_to_zazerkalie()
+
+        # test
+        if (h == 21 and m == 9 and s == 0):
+            Send_to_zazerkalie()
+
+        time.sleep(1)
+
+if __name__ == '__main__':
+    run()
