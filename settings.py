a9a695081d0eddd64da1a75c8a760beb04d4413a
ilovetiktiok <alexdubovyck@yandex.ru>
2023-02-17 02:35:07 +0300

    +logging

---
+++ settings.py
@@ -0,0 +1,10 @@
+# import os
+
+TOKEN = "5959614783:AAFKrPTe56Lg1lzh7RQA50hqfxTQRJXo6MA" #tg @amanda_lizard_bot
+
+CHAT_ID = -1001744761688 # @adobepremierpro2022 # для тестов
+
+# print("The keys and values of all environment variables:")
+# for key in os.environ:
+#     print(key, "=>", os.environ[key])
+#     print("The value of HOME is: ", os.environ["TMP"])
