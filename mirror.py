d1f4068b300506ac7f43d5b1b2f09fa393f913bb
ilovetiktiok <alexdubovyck@yandex.ru>
2023-02-17 02:35:05 +0300

    + logging

--- mirror.py
+++ mirror.py
@@ -1,6 +1,12 @@
-import time, requests, random
-from lib_1 import * # в конце - поменять!!!
+import time, requests, random, asyncio
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters
+# import telegram
+import logging
 
+logging.basicConfig(filename="E:\YandexDisk\Python\[2023] Amanda Lizard\log.log", level=logging.DEBUG)
+# logging.basicConfig(filename="log.log", level=logging.INFO) WARNING  ERROR CRITICAL
+
+from settings import * # в конце - поменять!!!
 # TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN") # print("TOKEN: ", TOKEN)
 
 # CHAT_ID = -1001670463029 # work
@@ -18,9 +24,8 @@ def Send_to_zazerkalie():
     requests.post(f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={text_for_send}")
 
 
-# await update.message.reply_text("I'm sorry Dave I'm afraid I can't do that.")
-
-def run():
+def task1():
+    logging.info("2й")
     while True:
         current_time = time.localtime()
         h, m, s = current_time.tm_hour, current_time.tm_min, current_time.tm_sec
@@ -41,6 +46,26 @@ def run():
 
         time.sleep(1)
 
+def greet(update, context):
+    text = update.message.text
+    print("text:", text)
+    # print("Вызывал /start") # /start
+    # update.message.reply_text(text)
+
+def task2():
+    print("1")
+    logging.info("1й")
+
+    mybot = Updater(TOKEN, update_queue=asyncio.Queue)
+
+    # dp = mybot.dispatcher
+    # mybot.add_handler(CommandHandler("start", greet))
+    mybot.MessageHandler(filters.Text , greet)
+
+    mybot.start_polling()
+
+
 
 if __name__ == '__main__':
-    run()
+    task2()
+    task1()
