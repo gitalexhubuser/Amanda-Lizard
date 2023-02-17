3f307bb61ef76e22e360da578f59da46239b5339
ilovetiktiok <alexdubovyck@yandex.ru>
2023-02-17 17:13:40 +0300

    Чистовик (финальный тест) прощай инфа из update

--- mirror.py
+++ mirror.py
@@ -1,15 +1,11 @@
 import time, requests, random
 from telegram.ext import MessageHandler, filters, ApplicationBuilder
-import logging
-
-logging.basicConfig(filename="E:\YandexDisk\Python\[2023] Amanda Lizard\log.log", level=logging.ERROR)
-# logging.basicConfig(filename="log.log", level=logging.INFO) WARNING  ERROR CRITICAL
-
 from settings import * # в конце - поменять!!!
 # TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN") # print("TOKEN: ", TOKEN)
 
-# CHAT_ID = -1001670463029 # work
-CHAT_ID = -1001729154400 # test
+
+CHAT_ID = -1001670463029 # work
+# CHAT_ID = -1001729154400 # test
 text_for_send = "man is weak" # человек слаб
 
 random_time_list1 = [5, 6, 7, 8]
@@ -24,8 +20,6 @@ def Send_to_zazerkalie():
 
 
 def task1():
-    logging.info("2й")
-    print("2й")
     while True:
         current_time = time.localtime()
         h, m, s = current_time.tm_hour, current_time.tm_min, current_time.tm_sec
@@ -48,56 +42,19 @@ def task1():
 
 
 
-
-
-
 async def message_handler(update, context):
-    print("update:", update)
-    # print(update.message.chat.id) -1001670463029
-
-    # if update.message.chat.title == "Зазеркалье твича":
     if update.message.chat.id == CHAT_ID:
         user_message = update.message.text
-        print("0 user_message:", user_message)
-        print("0 user_message:", member.full_name)
-
         if "миша" in user_message.lower():
             await update.message.reply_text('Но я не Миша!')
-    # else:
-    #     print("22:", update.message.chat.username)
-    #     print("33:", update.message.date)
-
-    #     print("1:", update.message.from_user.first_name)
-    #     print("2:", update.message.from_user.id) # effective_chat
-    #     print("3:", update.message.from_user.is_bot)
-    #     print("4:", update.message.from_user.last_name)
-    #     print("5:", update.message.from_user.username)
-    #     print(" ")
-
-
-    
-# async def task2():
-#     bot = telegram.Bot(TOKEN)
-#     async with bot:
-#         print(await bot.get_me())
-#         print((await bot.get_updates())[0])
-#         await bot.send_message(text='Hi John!', chat_id=CHAT_ID)
-#         print(await bot.get_my_default_administrator_rights(CHAT_ID))
-#         print(await bot.get_chat_administrators(chat_id=-1001670463029))
-#         print(await bot.get_chat_member_count(chat_id=-1001670463029))
 
 def task2():
-    print("1")
-    logging.info("1й")
-
     application = ApplicationBuilder().token(TOKEN).build()
-    # start_handler = MessageHandler(filters.TEXT, message_handler)
-    start_handler = MessageHandler(filters.StatusUpdate.NEW_CHAT_PHOTO, message_handler)
+    start_handler = MessageHandler(filters.TEXT, message_handler)
     application.add_handler(start_handler)
     application.run_polling()
 
 
 if __name__ == '__main__':
     task2()
-    # asyncio.run(task2())
     task1()
