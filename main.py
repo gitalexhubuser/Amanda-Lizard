b4a4bed9cce3f321584b1b10daeb6ca81126f32a
ilovetiktiok <alexdubovyck@yandex.ru>
2023-05-30 14:39:48 +0300

    + ветка Task-3

---
+++ main.py
@@ -0,0 +1,8 @@
+# from gtts import gTTS #pip install gtts
+# import speech_recognition as sr #pip install SpeechRecognition #pip install pyaudio
+
+def task3():
+    ...
+
+if __name__ == '__main__':
+    task3()
